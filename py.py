5329b98e2f210b3c7c026db27d0ff55545d7b472
vishapinnoji <76832335+vishapinnoji@users.noreply.github.com>
2021-01-03 05:05:38 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,29 @@
+
+
+
+a=25
+b=20
+a+=b    #(a=a+b)
+print(a)
+a-=b    #(a=a-b)
+print(a)
+a*=b    #(a=a*b)
+print(a)
+a**=b   #(a=a**b)
+print(a)
+a%=b    #(a=a%b)
+print(a)
+
+
+
+
+a="1,2,3,4,5"
+b="25,55,3,5"
+c={"25","5","41"}
+d=("vishal","sai","abhi","2020")
+print('2' in a) #true
+print("50" not in b) #true
+print("4" in b) # false
+print("41" in c) #true
+print("vishal" not in d) #false
+print("2020" in d) #true
