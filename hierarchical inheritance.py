b35a095cbd638885b982d3308f34b7b6b9f823ca
vishapinnoji <76832335+vishapinnoji@users.noreply.github.com>
2021-01-05 11:39:01 +0530

    Add files via upload

---
+++ hierarchical inheritance.py
@@ -0,0 +1,13 @@
+class A:
+    def m1(self):
+        print("vishal pinnoji")
+class B(A):
+    def m2(self):
+        print("pinn")
+class C(A):
+    def m2(self):
+        print("vish")
+c1=C()
+c1.m1()
+c1.m2()
+c1.m2()
