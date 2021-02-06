9c94600c7a595f376d9bffaa32c884ae79821441
vishapinnoji <76832335+vishapinnoji@users.noreply.github.com>
2021-02-06 18:03:14 +0530

    Add files via upload

---
+++ largestnumber.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number="))
+b=int(input("enter the second number="))
+c=int(input("enter the third number="))
+if a>=b>=c:
+    print("a is largest nymber")
+if b>=c>=a:
+    print("b is largest number")
+if c>=a>=b:
+    print("c is largest number")
---
+++ username.py
@@ -0,0 +1,12 @@
+while True:
+    a=str(input("enter the username="))
+    b=str(input("enter the password="))
+    if a=="vishal":
+        print("success...")
+        continue
+    if b=="123":
+        print("success.....")
+        continue
+    
+
+    
